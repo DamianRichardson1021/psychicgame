9a53ddc358224f93de27c4d9bca26d4b966535d4
Damian <exitexitgit config --global user.email richardson.damian@gmail.com>
2018-09-03 21:43:51 -0400

    needs more work on javascript and colors are better

---
+++ assets/logic.js
@@ -0,0 +1,16 @@
+
+
+var wins = 0;
+var losses = 0;
+
+var guessesLeft = 9;
+var possible = "abcdefghijklmnopqrstuvwxyz";
+var secret = possible.charAt(Math.floor(Math.random() * possible.length));
+
+var winsText = document.getElementById("wins").textContent;
+document.getElementById("losses").textContent = 0
+console.log (document.getElementById("wins")) 
+winsText = wins;
+//lossesText = losses; 
+
+console.log (wins)
